f4a045cf7fbebdc83001f388d96454742eed5173
Prakash Kumar Mallik <mallikprakashkumar56@gmail.com>
2026-08-06 18:48:25 +0530

    Time: 2 ms (99.32%) | Memory: 44 MB (96.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right)
+        {
+            char ch1=s.charAt(left);
+            char ch2=s.charAt(right);
+            if(!Character.isLetterOrDigit(ch1))
+            {
+                left++;
+                continue;
+            }
+            else
+            {
+                ch1=Character.toLowerCase(ch1);
+                
+            }
+            if(!Character.isLetterOrDigit(ch2))
+            {
+                right--;
+                continue;
+            }
+            else{
+                ch2=Character.toLowerCase(ch2);
+            }
+
+            if(ch1!=ch2)
+            {
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+}
