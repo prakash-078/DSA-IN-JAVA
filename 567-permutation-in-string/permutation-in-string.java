f901c3979adcaa7bf0cbc3cff609b8bc05f94938
Prakash Kumar Mallik <mallikprakashkumar56@gmail.com>
2026-08-15 20:19:20 +0530

    Time: 7 ms (68.73%) | Memory: 44.1 MB (47.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length()<s1.length())
+        if(s1.length()>s2.length())
         {
             return false;
         }
         int[] freq1=new int[26];
         int[] freq2=new int[26];
+
         for(int i=0;i<s1.length();i++)
         {
             freq1[s1.charAt(i)-'a']++;
             freq2[s2.charAt(i)-'a']++;
         }
-
         if(matches(freq1,freq2))
         {
             return true;
@@ -26,10 +26,7 @@ class Solution {
                 return true;
             }
         }
-        
         return false;
-
-
     }
     public boolean matches(int[] freq1,int[] freq2)
     {
